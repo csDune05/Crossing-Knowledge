f0b7d11a45ee6cf7b57a9425cc49a101560e8b91
csDune05 <dungduoc123bn@gmail.com>
2025-12-13 18:00:00 +0700

    [add] empty page to test layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,17 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "./pages/HomePage.jsx";
+import AppLayout from "./layouts/AppLayout.jsx";
+import DashboardPage from "./pages/DashboardPage.jsx";
+import VocabPage from "./pages/VocabPage.jsx";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route element={<AppLayout />}>
+        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/vocab" element={<VocabPage />} />
+      </Route>
     </Routes>
   );
 };
